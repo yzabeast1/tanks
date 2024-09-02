821ac7865f7b0f70577f3893dfe989597c8b88ed
yzabeast1 <yzabeast1@gmail.com>
2024-09-01 20:11:36 -0500

    added draw 2 card code

---
+++ server/cards/draw-2.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+module.exports = function (req, res, cardid) {
+    fs.readFile('games.json', 'utf8', (err, data) => {
+        const username = req.headers.username
+        const game = req.headers.joincode
+        var games=JSON.parse(data)
+        games[game]['players'][username]['hand'].push(games[game]['draw_pile'][0])
+        games[game]['players'][username]['hand'].push(games[game]['draw_pile'][1])
+        games[game]['draw_pile'].splice(0,2)
+        const handIndex = games[game]['players'][username]['hand'].indexOf(cardid)
+        games[game]['players'][username]['hand'].splice(handIndex, 1)
+        fs.writeFile('games.json', JSON.stringify(games, null, "\t"), function (err) { if (err) console.log(err) })
+    })
+}
