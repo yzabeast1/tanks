cd2667f0251de64261a13b2c263c0f7c791c0ae9
yzabeast1 <yzabeast1@gmail.com>
2024-09-03 23:04:27 -0500

    added crack code

---
+++ server/cards/crack.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const removeIfDead=require("./otherFunctions/removeIfDead.js")
+module.exports = function (req, res, cardid) {
+    fs.readFile('games.json', 'utf8', (err, data) => {
+        var games = JSON.parse(data)
+        var game = req.headers.joincode
+        var target = req.headers.target
+        const username = req.headers.username
+        games[game]['players'][target]['health']-=2
+        const handIndex = games[game]['players'][username]['hand'].indexOf(cardid)
+        games[game]['players'][username]['hand'].splice(handIndex, 1)
+        games=removeIfDead(games, game, target)
+        fs.writeFile('games.json', JSON.stringify(games, null, "\t"), function (err) { if (err) console.log(err) })
+    })
+}
