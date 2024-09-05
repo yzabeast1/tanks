1f105d2eacc95f9dfaa1ab40bf0cd6f97200b44b
yzabeast1 <yzabeast1@gmail.com>
2024-09-05 13:33:43 -0500

    added last stand as auto activation on death

--- server/otherFunctions/removeAllIfDead.js
+++ server/otherFunctions/removeAllIfDead.js
@@ -1,9 +1,15 @@
-module.exports=function(games,game){
+const lastStandID = 17
+module.exports = function (games, game) {
 	if (games[game] && games[game].players) {
 		const players = games[game].players;
 		for (const player in players) {
-			if (players[player].health === 0) {
-				delete players[player];
+			if (players[player].health <= 0) {
+				if (players[player]['hand'].includes(lastStandID)) {
+					players[player]['health'] = 1
+					const handIndex = playerss[player]['hand'].indexOf(lastStandID)
+					players[player]['hand'].splice(handIndex, 1)
+				}
+				else delete players[player];
 			}
 		}
 	}
--- server/otherFunctions/removeIfDead.js
+++ server/otherFunctions/removeIfDead.js
@@ -1,5 +1,11 @@
-module.exports=function(games,code,target){
-    if(games[code]['players'][target]['health']<=0)
-        delete games[code]['players'][target]
+const lastStandID=17
+module.exports = function (games, game, target) {
+    if (games[game]['players'][target]['health'] <= 0)
+        if (games[game]['players'][target]['hand'].includes(lastStandID)) {
+            games[game]['players'][target]['health'] = 1
+            const handIndex = games[game]['players'][username]['hand'].indexOf(lastStandID)
+            games[game]['players'][username]['hand'].splice(handIndex, 1)
+        }
+        else delete games[game]['players'][target]
     return games
 }
