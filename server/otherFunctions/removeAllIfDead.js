1920346736e2ca01f33fd459b07f03f9d44554c8
yzabeast1 <yzabeast1@gmail.com>
2024-09-25 00:14:31 -0500

    fix remove all if dead to use remove if dead

--- server/otherFunctions/removeAllIfDead.js
+++ server/otherFunctions/removeAllIfDead.js
@@ -1,17 +1,11 @@
-const lastStandID = 17
+const removeIfDead = require("./removeIfDead.js")
+
 module.exports = function (games, game) {
-	if (games[game] && games[game].players) {
-		const players = games[game].players;
-		for (const player in players) {
-			if (players[player].health <= 0) {
-				if (players[player]['hand'].includes(lastStandID)) {
-					players[player]['health'] = 1
-					const handIndex = playerss[player]['hand'].indexOf(lastStandID)
-					players[player]['hand'].splice(handIndex, 1)
-				}
-				else delete players[player];
-			}
+	if (games[game] && games[game]['players']) {
+		var players = games[game]['order']
+		for (var i = 0; i < players.length; i++) {
+			removeIfDead(games, game, players[i])
 		}
 	}
-	return games;
+	return games
 }
