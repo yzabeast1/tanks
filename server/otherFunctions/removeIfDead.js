0623771ba879694820f7d971cd93eec9cb079d38
yzabeast1 <yzabeast1@gmail.com>
2024-09-25 00:10:24 -0500

    fix last stand not working

--- server/otherFunctions/removeIfDead.js
+++ server/otherFunctions/removeIfDead.js
@@ -1,18 +1,27 @@
-const lastStandID = 17
+const fs = require('fs')
 module.exports = function (games, game, target) {
-    if (games[game]['players'][target]['health'] <= 0)
-        if (games[game]['players'][target]['hand'].includes(lastStandID)) {
-            games[game]['players'][target]['health'] = 1
-            const handIndex = games[game]['players'][username]['hand'].indexOf(lastStandID)
-            games[game]['players'][username]['hand'].splice(handIndex, 1)
-        }
-        else {
-            delete games[game]['players'][target]
-            var currentPlayer = games[game]['order'][games[game]['turn']]
-            if (currentPlayer != target) {
-                games[game]['order'].splice(games[game]['order'].indexOf(target))
-                games[game]['turn'] = games[game]['order'].indexOf(currentPlayer)
+    if (games[game]['players'][target]['health'] <= 0) {
+        fs.readFile('deck.json', 'utf8', (err, data) => {
+            var deck = JSON.parse(data)
+            var lastStandID=-1
+            for(var i=0;i<deck.length;i++)if(deck[i]['id']=='last-stand')lastStandID=i
+            console.log(lastStandID)
+            if (lastStandID!=-1&&games[game]['players'][target]['hand'].includes(lastStandID)) {
+                games[game]['players'][target]['health'] = 1
+                const handIndex = games[game]['players'][username]['hand'].indexOf(lastStandID)
+                games[game]['players'][username]['hand'].splice(handIndex, 1)
             }
-        }
-    return games
+            else {
+                delete games[game]['players'][target]
+                var currentPlayer = games[game]['order'][games[game]['turn']]
+                if (currentPlayer != target) {
+                    games[game]['order'].splice(games[game]['order'].indexOf(target))
+                    games[game]['turn'] = games[game]['order'].indexOf(currentPlayer)
+                }
+            }
+
+        })
+        return games
+    }
+    else return games
 }
