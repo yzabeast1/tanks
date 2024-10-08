adb24365d63e18cf8cc6e674d4ba5038026a3246
yzabeast1 <yzabeast1@gmail.com>
2024-10-08 13:38:28 -0500

    updated playCard to use readFileSync

--- server/commands/playcard.js
+++ server/commands/playcard.js
@@ -2,17 +2,13 @@ const fs = require('fs')
 module.exports = function (req, res) {
     const cardID = req.headers.cardid
     const username = req.headers.username
-    const joincode=req.headers.joincode
-    fs.readFile('deck.json', 'utf8', (err, data) => {
-        fs.readFile('games.json', 'utf8', (err1, data1) => {
-            const game=JSON.parse(data1)[joincode]
-            if (username==game['order'][game['turn']]) {
-                const deck = JSON.parse(data)
-                const card = deck[cardID]
-                const cardFunction = require("../" + card['code-location'])
-                cardFunction(req, res, cardID)
-            }
-            res.end()
-        })
-    })
+    const game = req.headers.joincode
+    const deck = JSON.parse(fs.readFileSync('deck.json', 'utf8'))
+    var games = JSON.parse(fs.readFileSync('games.json', 'utf8'))
+    if (username == games[game]['order'][games[game]['turn']]) {
+        const card = deck[cardID]
+        const cardFunction = require("../" + card['code-location'])
+        cardFunction(req, res, cardID)
+    }
+    res.end()
 }
